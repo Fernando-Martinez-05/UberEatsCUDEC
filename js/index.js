d97895a6b909383e84224ca999333785b427fd1d
Luis Merlín <luisfernandomartinezmerlin@gmail.com>
2026-06-04 21:25:08 -0600

    Agregamos main card- precio y botón de eliminar

--- js/index.js
+++ js/index.js
@@ -1,14 +1,41 @@
-btnAgregarPlatillo = document .getElementById('btnAgregarPlatillo');
+let contenido = '';
 
 document.addEventListener('DOMContentLoaded', function() {
-  // nav menu
-  const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, {edge: 'right'});
-  // add recipe form
-  const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
+// nav menu
+const menus = document.querySelectorAll('.side-menu');
+M.Sidenav.init(menus, {edge: 'right'});
+// add recipe form
+const forms = document.querySelectorAll('.side-form');
+M.Sidenav.init(forms, {edge: 'left'});
 });
 
 btnAgregarPlatillo.addEventListener('click', function(){
 alert('Platillo agregado')
 });
+
+function mostrarPlatillo(Platillo, id){
+contenido += `
+<div class = "card-panel recipe white row"
+ data-id="${id}"> 
+<div class = "recipe-details">
+<div class = "recipe-title">
+${Platillo.nombre}
+</div>
+
+<div class = "recipe-ingredients">
+${Platillo.ingredientes}
+</div>
+
+<div class = "recipe-Precio">
+${Platillo.Precio}
+</div>
+
+
+</div>
+<div class = "recipe-delete">
+<i class = "material-icons"  data-id="${id}">delete_outline</i>
+
+</div>`;
+document.querySelector('.recipes').innerHTML = contenido;
+
+}
