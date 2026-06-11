426eaad153ec2bdf672fadcf29e8aa44b24798c5
Enpega <enrique.perez.garibay@gmail.com>
2026-06-10 19:22:28 -0600

    Se muestran platillos nuevos y los cambios de estos, sin duplicar.

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ btnAgregarPlatillo.addEventListener('click', function() {
 
 function mostrarPlatillo(platillo, id) {
     contenido = `
-    <div class='card-panel recipe white row'>
+    <div class='card-panel recipe white row' id='${id}'>
          <div class='recipe-details'>
                 <div class='recipe-title'>
                     ${platillo.nombre}
@@ -25,8 +25,23 @@ function mostrarPlatillo(platillo, id) {
                 <div class='recipe-ingredients'>
                     ${platillo.ingredientes}
                 </div>
+                <div class="recipe-price">
+                    ${platillo.precio}
+                </div>
+                <div class="recipe-delete">
+                    <i class="material-icons" data-id='${id}'>
+                    delete_outline
+                    </i>
+                </div>
         </div>
       </div>
     `;
     document.querySelector(".recipes").innerHTML += contenido;
+}
+
+function actualizarPlatillo(platillo, id) {
+    let tarjeta = document.getElementById(`${id}`);
+    tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+    tarjeta.querySelector(".recipe-ingredients").innerHTML = platillo.ingredientes;
+    tarjeta.querySelector(".recipe-price").innerHTML = platillo.precio;
 }
